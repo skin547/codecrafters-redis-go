5b7a5092ad1f4b190a6a5812d8679fd9ad295bd1
skin547 <skin547yo@gmail.com>
2024-04-06 16:10:50 +0800

    refactor: change file structure, move Store to internal/store

--- app/server.go
+++ app/server.go
@@ -8,51 +8,11 @@ import (
 	"io"
 	"math/rand"
 	"net"
+	store "redis-go/internal"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
-
-type Store struct {
-	db  sync.Map
-	exp sync.Map
-}
-
-func NewStore() *Store {
-	store := &Store{db: sync.Map{}, exp: sync.Map{}}
-	return store
-}
-
-func (k *Store) Get(key string) (string, bool) {
-	value, ok := k.db.Load(key)
-	if !ok {
-		return "", false
-	}
-	expiration, expOk := k.exp.Load(key)
-	if expOk {
-		expTime := expiration.(int64)
-		now := time.Now().UnixNano() / int64(time.Millisecond)
-		if expTime < now {
-			k.db.Delete(key)
-			k.exp.Delete(key)
-			return "", false
-		}
-	}
-	return value.(string), ok
-}
-
-func (k *Store) Set(key string, value string) string {
-	k.db.Store(key, value)
-	return "OK"
-}
-
-func (k *Store) SetPx(key string, value string, exp int64) string {
-	now := time.Now().UnixNano() / int64(time.Millisecond)
-	k.db.Store(key, value)
-	k.exp.Store(key, now+exp)
-	return "OK"
-}
 
 type ReplicaConfig struct {
 	masterHost    string
@@ -167,7 +127,7 @@ func main() {
 	defer l.Close()
 
 	fmt.Println("Initialize key value store...")
-	store := NewStore()
+	store := store.NewStore()
 	for {
 		conn, err := l.Accept()
 		if err != nil {
@@ -180,7 +140,7 @@ func main() {
 	}
 }
 
-func handle(conn net.Conn, store *Store) {
+func handle(conn net.Conn, store *store.Store) {
 	fmt.Println("accept a request, addr:", conn.RemoteAddr().String())
 	defer conn.Close()
 
---
+++ internal/store.go
@@ -0,0 +1,46 @@
+package store
+
+import (
+	"sync"
+	"time"
+)
+
+type Store struct {
+	db  sync.Map
+	exp sync.Map
+}
+
+func NewStore() *Store {
+	store := &Store{db: sync.Map{}, exp: sync.Map{}}
+	return store
+}
+
+func (k *Store) Get(key string) (string, bool) {
+	value, ok := k.db.Load(key)
+	if !ok {
+		return "", false
+	}
+	expiration, expOk := k.exp.Load(key)
+	if expOk {
+		expTime := expiration.(int64)
+		now := time.Now().UnixNano() / int64(time.Millisecond)
+		if expTime < now {
+			k.db.Delete(key)
+			k.exp.Delete(key)
+			return "", false
+		}
+	}
+	return value.(string), ok
+}
+
+func (k *Store) Set(key string, value string) string {
+	k.db.Store(key, value)
+	return "OK"
+}
+
+func (k *Store) SetPx(key string, value string, exp int64) string {
+	now := time.Now().UnixNano() / int64(time.Millisecond)
+	k.db.Store(key, value)
+	k.exp.Store(key, now+exp)
+	return "OK"
+}
